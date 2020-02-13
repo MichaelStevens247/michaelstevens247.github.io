3b97c02613d126427d42b11ce6d51c6a9a282b57
Michael Stevens <49499727+MichaelStevens247@users.noreply.github.com>
2020-02-13 10:50:32 -0600

    Uploading projects for final version of portfolio

---
+++ Rodeo Mexican Restaurant/script.js
@@ -0,0 +1,102 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     var curIndex = 0,
+//     imgDuration = 3000,
+//     slider = document.getElementById("slider"),
+//     slides = slider.childNodes;
+
+// imgArray = [
+// 'beer buckets.jpg',
+// "beer and rita's.jpg",
+// 'fried shrimp.jpg',
+// 'beer and bloody mary.jpg',
+// 'tamales.jpg'
+// ];
+
+// function buildSlideShow(arr) {
+//     for (i = 1; i < arr.length; i++) {
+//         var img = document.createElement('img');
+//         img.src = arr[i];    
+//         slider.appendChild(img);
+//     }
+// }
+
+// // var imgMargin = (img.offsetWidth) / 2;
+// // img.style.left = '50%';
+// // img.style.marginLeft = imgMargin * -1;
+
+// function slideShow() {
+    
+//         function fadeIn(e) {
+//             e.className = "fadeIn";
+//         };
+    
+//         function fadeOut(e) {
+//             e.className = "";
+//         };
+
+//         // debugger;
+
+//                 fadeOut(slides[curIndex]);
+//                 curIndex++;
+//                 if (curIndex === slides.length) {
+//                     curIndex = 1;
+//                 }
+                
+//                 fadeIn(slides[curIndex]);
+        
+//                 setTimeout(function () {
+//                     slideShow();
+//                 }, imgDuration);
+//             };
+
+//             buildSlideShow(imgArray);
+//             slideShow();
+        
+
+        
+
